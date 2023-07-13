aa93ffa07ca50aaf2c02e6409edd4e402dfacc08
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-13 23:25:45 +0900

    chore: staged changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import node from "@astrojs/node";
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true
+    assets: true,
   },
   integrations: [tailwind()],
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
-});
+    mode: 'middleware',
+  }),
+})
