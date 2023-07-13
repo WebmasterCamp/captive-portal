a53e24213fc90f0f65a02ea4a6ada3e5a3612300
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-13 23:07:23 +0900

    ci: initialize ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
